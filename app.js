95c99609f27b174d461523cbd8399486235255b5
Aftabe <shamsasghari77@gmail.com>
2020-08-25 17:33:38 +1000

    imediatly invoked function is setted

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-function openNav() {
+(function(){
+  function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
   
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+}());
