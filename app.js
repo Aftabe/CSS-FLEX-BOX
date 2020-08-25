f7b9b09f1a0faf3f8e17e80ac1fb5bc8a0ad9c4b
Aftabe <shamsasghari77@gmail.com>
2020-08-25 17:51:17 +1000

    js statemens used

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 (function(){
-  function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
-}());
+  let name,
+  ouline;
+  name = prompt('What\'s your name');
+  ouline = name.valueOf(name);
+  alert('Welcome ' + ouline);
+}())
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
